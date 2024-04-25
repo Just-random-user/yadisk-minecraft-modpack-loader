fbe46300b46887c1e3d12d9fd86e4f7ab9b94776
Пользователь <sineokovivan@mail.ru>
2024-04-25 19:03:03 +0300

    add venv support

--- modupdate.bat
+++ modupdate.bat
@@ -1,1 +1,4 @@
-py python\jaru_modupdate.py "uT32vpVyWz4wHQ" "/LetsFlame's Create 1.0/mods"
+pip install requests
+py -m venv python\venv
+python\venv\bin\pip3 install requests | find /V "already satisfied"
+python\venv\bin\python python\jaru_modupdate.py "uT32vpVyWz4wHQ" "/LetsFlame's Create 1.0/mods"
