dfd4d101c5714156fcf7184d3f2b1268ff590394
Пользователь <sineokovivan@mail.ru>
2024-04-27 17:28:12 +0300

    fix some windows compatibility issues

--- modpack_download.bat
+++ modpack_download.bat
@@ -1,3 +1,3 @@
-py -m venv python\venv
+python -m venv python\venv
 python\venv\Scripts\pip3.exe install requests | find /V "already satisfied"
 python\venv\Scripts\python.exe python\jaru_download.py "uT32vpVyWz4wHQ" "/LetsFlame's Create 1.0"
